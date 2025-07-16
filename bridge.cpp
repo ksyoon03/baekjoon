6f929c71736a5e506caeab17cd8fa25ca9ba4c41
ksyoon03 <godwoo09@gmail.com>
2025-07-16 11:09:57 +0900

    Add files via upload

--- bridge.cpp
+++ bridge.cpp
@@ -1,33 +1,31 @@
 #include <iostream>
 using namespace std;
 
-long long fac(int n) {
+int fac(int n, int m) {
+	if (n < 0 || n > m)
+		return 0;
+	if (n == m || n == 0)
+		return 1;
+	if (n > m / 2)
+		n = m - n;
+
 	int result = 1;
-	for (int i = 2; i <= n; i++) {
-		result *= i;
+	for (int i = 0; i < n; i++) {
+		result *= (m - i);
+		result /= (i + 1);
 	}
 	return result;
 }
 
 int main() {
-	int t;
+	int t, n, m;
 	cin >> t;
-
-	int* n = new int[t];		// N
-	int* n_fac = new int[t];	// N ÆÑÅä¸®¾ó
-	int* m = new int[t];		// M
-	int* m_fac = new int[t];	// M ÆÑÅä¸®¾ó
-	int* mn_fac = new int[t];	// M - N ÆÑÅä¸®¾ó
-	int* result = new int[t];	// mCn
-
+	int* result = new int[t];
 	for (int i = 0; i < t; i++) {
-		cin >> n[i] >> m[i];
-		n_fac[i] = fac(n[i]);
-		m_fac[i] = fac(m[i]);
-		mn_fac[i] = fac(m[i] - n[i]);
-		result[i] = m_fac[i] / (n_fac[i] * mn_fac[i]);
+		cin >> n >> m;
+		result[i] = fac(n, m);
 	}
-	
+
 	for (int i = 0; i < t; i++) {
 		cout << result[i] << endl;
 	}
